1e1a19cf15e9d2aeb0431805e27db5d71fdd002c
victor acrani ruivo <victoracrani@gmail.com>
2022-04-19 14:34:48 -0300

    Adicionando exercícios da aula 3.

---
+++ br.com.class.lestcode/src/aula03/Exercicio05.java
@@ -0,0 +1,35 @@
+package aula03;
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        double altura;
+        double peso;
+
+        System.out.println("Digite a altura da pessoa.");
+        altura = sc.nextDouble();
+        System.out.println("Digite o peso da pessoa.");
+        peso = sc.nextDouble();
+
+        sc.close();
+
+        double imc = peso / Math.pow(altura, 2);
+
+        System.out.println("IMC = " + imc);
+
+        if (imc < 18.5) {
+            System.out.println("A pessoa está abaixo do peso");
+        } else if (imc > 18.5 && imc < 24.9) {
+            System.out.println("A pessoa está com peso normal");
+        } else if (imc > 25.0 && imc < 29.9 ) {
+            System.out.println("A pessoa está com excesso de peso");
+        } else if (imc >= 30.0) {
+            System.out.println("A pessoa está obesa");
+        }
+    }
+}
+
---
+++ br.com.class.lestcode/src/aula03/Exercicio10.java
@@ -0,0 +1,38 @@
+package aula03;
+
+import java.util.Scanner;
+
+public class Exercicio10 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int numeroInserido;
+        boolean flagAcertou = false;
+        int valorMaximo = 10;
+        int valorMinimo = 1;
+        int numeroAleatorio = (int) ((Math.random() * (valorMaximo - valorMinimo)) + valorMinimo);
+
+        System.out.println("Um número entre 1 a 10 foi gerado. Tente adivinhar qual é. Você tem 3 tentativas.");
+
+        for(int i = 0; i < 3; i++) {
+            numeroInserido = sc.nextInt();
+
+            if(numeroInserido == numeroAleatorio) {
+                flagAcertou = true;
+                break;
+            } else if (numeroInserido > numeroAleatorio) {
+                System.out.println("O número aleatório é menor. tente novamente.");
+            } else {
+                System.out.println("O número aleatório é maior. tente novamente.");
+            }
+        }
+        sc.close();
+
+        if(flagAcertou) {
+            System.out.println("Parabéns. Você acertou!");
+        } else {
+            System.out.println("Você perdeu T.T");
+        }
+    }
+}
