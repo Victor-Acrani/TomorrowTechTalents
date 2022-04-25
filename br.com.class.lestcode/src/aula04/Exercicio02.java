2ed8a441717e2de83fe332978f120769af473506
victor acrani ruivo <victoracrani@gmail.com>
2022-04-25 18:08:00 -0300

    Adicionando exercícios da aula 4.

---
+++ br.com.class.lestcode/src/aula04/Exercicio02.java
@@ -0,0 +1,26 @@
+package aula04;
+
+import java.util.Scanner;
+
+public class Exercicio02 {
+
+    public static void main(String[] args) {
+        System.out.println("Insira 10 valores:");
+        Scanner sc = new Scanner(System.in);
+        double[][] matriz = new double[2][5];
+
+        for (int i = 0; i < 2; i++){
+            for(int j = 0; j < 5; j++){
+                matriz[i][j] = sc.nextDouble();
+            }
+        }
+        sc.close();
+
+        for (int i = 0; i < 2; i++){
+            for(int j = 0; j < 5; j++){
+                System.out.printf("%4.2f ",matriz[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ br.com.class.lestcode/src/aula04/Exercicio07.java
@@ -0,0 +1,43 @@
+package aula04;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Exercicio07 {
+
+    public static void main(String[] args) {
+
+        char[] gabarito = new char[13];
+        Random random = new Random();
+        for(int i = 0; i < 13; i++){
+            gabarito[i] = (char)(random.nextInt(4)+97);
+        }
+
+        System.out.println("Insira sua aposta de 13 números. Inserir apenas 1, 2 ou 3.");
+        Scanner sc = new Scanner(System.in);
+        String[] resposta = new String[13];
+        int acertos = 0;
+
+        for(int i = 0; i < 13; i++){
+            resposta[i] = sc.next();
+            if(resposta[i].charAt(0) == gabarito[i]){
+                acertos++;
+            }
+        }
+        sc.close();
+
+        for(int i = 0; i < 13; i++){
+            System.out.printf("%s ",gabarito[i]);
+        }
+        System.out.println();
+        for(int i = 0; i < 13; i++){
+            System.out.printf("%s ",resposta[i]);
+        }
+        if(acertos >= 6){
+            System.out.printf("%nParabéns! Você ganhou!");
+        }else {
+            System.out.printf("%nVocê acertou %d números", acertos);
+        }
+
+    }
+}
